4b5b4c3d60b66cfb27d89ff8c8e4f80c28baa89d
theaton89 <taylornheaton@gmail.com>
2021-05-30 14:21:01 -0500

    Added css and search bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import "./App.css";
 import Weather from "./Weather";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
       <Weather />
+      <p>
+        This project was coded by{" "}
+        <a
+          href="https://www.youtube.com/channel/UC0rQDkXDNeidKnUafldmpdA"
+          target="_blank"
+        >
+          Taylor Heaton
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/theaton89/final-project-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a href="https://jovial-bose-b591a2.netlify.app" target="_blank">
+          hosted on Netlify
+        </a>
+        .
+      </p>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,56 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Weather.css";
 
 export default function Weather() {
-  return <div className="Weather">Hello</div>;
+  return (
+    <div className="Weather" class="shadow p-3 mb-5 bg-white rounded">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              autoFocus="on"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Sunday, May 30</li>
+        <li>Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+              alt="cloudy"
+              className="float-left"
+            />
+            <span className="float-left">
+              <span className="temperature">80</span>
+              <span className="unit">°F</span>
+            </span>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: 32%</li>
+            <li>Wind: 13mph</li>
+            <li>Precipitation: 23%</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
