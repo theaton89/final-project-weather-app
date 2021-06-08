5aac1c01a2c3ff966e1c5e6e1e550875a48c41db
theaton89 <taylornheaton@gmail.com>
2021-06-08 08:49:55 -0500

    updated Weather.css after deleting celsius conversion

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -25,6 +25,8 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
